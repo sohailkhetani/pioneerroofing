6983344e1ccd542147ae8b63323b5453b073029c
Serge B <sergebaidin@gmail.com>
2016-05-30 20:59:02 +0300

    May 30 updates

--- markup/assets/js/main.js
+++ markup/assets/js/main.js
@@ -19,7 +19,7 @@ function counter(){
 			'<span class="dot <%= label %>"></span>';
 	//$('#counter-template').html(tmp)
 	var labels = ['weeks', 'days', 'hours', 'minutes', 'seconds'],
-		countDate = '2016/05/25',
+		countDate = '2016/05/31',
 		template = _.template(tmp),
 		currDate = '00:00:00:00:00',
 		nextDate = '00:00:00:00:00',
@@ -115,87 +115,73 @@ function same_height(){
 }
 
 function overlay_menu(){
-	var overlayNav = $('.cd-overlay-nav'),
-		overlayContent = $('.cd-overlay-content'),
-		navigation = $('.cd-primary-nav'),
-		toggleNav = $('.cd-nav-trigger');
+	var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
 
-	//inizialize navigation and content layers
-	layerInit();
-	$(window).on('resize', function(){
-		window.requestAnimationFrame(layerInit);
-	});
-
-	//open/close the menu and cover layers
-	toggleNav.on('click', function(){
-		if(!toggleNav.hasClass('close-nav')) {
-			//it means navigation is not visible yet - open it and animate navigation layer
-			toggleNav.addClass('close-nav');
-			
-			overlayNav.children('span').velocity({
-				translateZ: 0,
-				scaleX: 1,
-				scaleY: 1,
-			}, 500, 'easeInCubic', function(){
-				navigation.addClass('fade-in');
-			});
-		} else {
-			//navigation is open - close it and remove navigation layer
-			toggleNav.removeClass('close-nav');
-			
-			overlayContent.children('span').velocity({
-				translateZ: 0,
-				scaleX: 1,
-				scaleY: 1,
-			}, 500, 'easeInCubic', function(){
-				navigation.removeClass('fade-in');
-				
-				overlayNav.children('span').velocity({
-					translateZ: 0,
-					scaleX: 0,
-					scaleY: 0,
-				}, 0);
-				
-				overlayContent.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-					overlayContent.children('span').velocity({
-						translateZ: 0,
-						scaleX: 0,
-						scaleY: 0,
-					}, 0, function(){overlayContent.removeClass('is-hidden')});
-				});
-				if($('html').hasClass('no-csstransitions')) {
-					overlayContent.children('span').velocity({
-						translateZ: 0,
-						scaleX: 0,
-						scaleY: 0,
-					}, 0, function(){overlayContent.removeClass('is-hidden')});
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
 				}
-			});
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
 		}
-	});
-
-	function layerInit(){
-		var diameterValue = (Math.sqrt( Math.pow($(window).height(), 2) + Math.pow($(window).width(), 2))*2);
-		overlayNav.children('span').velocity({
-			scaleX: 0,
-			scaleY: 0,
-			translateZ: 0,
-		}, 50).velocity({
-			height : diameterValue+'px',
-			width : diameterValue+'px',
-			top : -(diameterValue/2)+'px',
-			left : -(diameterValue/2)+'px',
-		}, 0);
-
-		overlayContent.children('span').velocity({
-			scaleX: 0,
-			scaleY: 0,
-			translateZ: 0,
-		}, 50).velocity({
-			height : diameterValue+'px',
-			width : diameterValue+'px',
-			top : -(diameterValue/2)+'px',
-			left : -(diameterValue/2)+'px',
-		}, 0);
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+		}
 	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
 }
+
+// function overlay_menu(){
+// 	var animOptions = {
+//           duration: 250,
+//           easing: "ease-in-out"
+//       };
+//       var open = false;
+//       $(".js-toggle").on("click", function() {
+//           var $el = $(this);
+//           var $rects = $el.find("svg rect");
+//           if ($el.hasClass("active")) {
+//               $rects.eq(1).velocity("stop").velocity("reverse");
+//               $rects.eq(0).add($rects.eq(2)).velocity("stop").velocity("reverse");
+//           } else {
+//               $rects.eq(1).velocity({
+//                   opacity: 0
+//               }, animOptions);
+//               $rects.eq(0).velocity({
+//                   transformOrigin: [30, 25],
+//                   translateY: 17,
+//                   rotateZ: 45
+//               }, animOptions);
+//               $rects.eq(2).velocity({
+//                   transformOrigin: [30, 25],
+//                   translateY: -17,
+//                   rotateZ: -45
+//               }, animOptions);
+//           }
+//           $el.toggleClass("active");
+//       });
+// }
